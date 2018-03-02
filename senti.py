778df2710ac8d2ed5e59cadd13a2be7e25e10040
S.KRISHNAN <skrish22195@gmail.com>
2018-03-03 03:58:08 +0530

    initial commit

---
+++ senti.py
@@ -0,0 +1,22 @@
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import json
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/getSentiment',methods = ['GET','POST'])
+def getSentiment():
+    arrSubs = request.json
+    arrSentiment = []
+    for sub in arrSubs:
+        print(sub)
+        text = TextBlob(sub)
+        arrSentiment.append(round(text.sentiment[0],2))
+    print(arrSentiment);    
+    return jsonify({"status":'success',"sentiment":arrSentiment})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
